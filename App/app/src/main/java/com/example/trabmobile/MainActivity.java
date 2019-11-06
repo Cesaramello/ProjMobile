2c4f4008283d39e8c68ca155aef95448b65bfa92
César Augusto <cezinha_mello@hotmail.com>
2019-11-06 17:43:02 -0400

    não sei

--- App/app/src/main/java/com/example/trabmobile/MainActivity.java
+++ App/app/src/main/java/com/example/trabmobile/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.trabmobile;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("fófócom");
+
+        myViewPager = (ViewPager) findViewById(R.id.tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 }
