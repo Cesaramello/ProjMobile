3e9e4b6026c8de21e5febb1e20e290aa20a3adda
César Augusto <cezinha_mello@hotmail.com>
2019-11-08 02:03:18 -0400

    criação do login (com um erro de navegação)

--- App/app/src/main/java/com/example/trabmobile/LoginActivity.java
+++ App/app/src/main/java/com/example/trabmobile/LoginActivity.java
@@ -4,17 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEmail, UserPassword;
+    private TextView NeedNewAccountLink, ForgetPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void InitializeFields() {
+        LoginButton = (Button) findViewById(R.id.login_button);
+        PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_Link);
+        ForgetPasswordLink = (TextView) findViewById(R.id.forget_password_Link);
     }
 
     @Override
@@ -29,5 +54,10 @@ public class LoginActivity extends AppCompatActivity {
     private void sendUserToMainActivity() {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent );
+    }
+
+    private void sendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent );
     }
 }
