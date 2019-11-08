ba9683f61b91ef2cac6eb8e34a6610f188dcc17d
César Augusto <cezinha_mello@hotmail.com>
2019-11-08 00:17:14 -0400

    prévias das telas de login

--- App/app/src/main/java/com/example/trabmobile/MainActivity.java
+++ App/app/src/main/java/com/example/trabmobile/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager myViewPager;
     private TabLayout myTabLayout;
     private TabsAccessorAdapter myTabsAccessorAdapter;
+
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +36,21 @@ public class MainActivity extends AppCompatActivity {
 
         myTabLayout = (TabLayout) findViewById(R.id.tabs);
         myTabLayout.setupWithViewPager(myViewPager);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser == null){
+            sendUserToLoginActivity();
+        }
+    }
+
+
+    private void sendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent );
     }
 }
