21b38eeafd1032f36fd3da33661be2c063b1893f
Gabriel Cansanção <gabrielkansan@gmail.com>
2019-11-16 19:40:43 -0400

    Add ChatsFragment 1.2

--- App/app/src/main/java/com/example/trabmobile/ChatsFragment.java
+++ App/app/src/main/java/com/example/trabmobile/ChatsFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
